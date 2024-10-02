372f40ab5c126b3eb3552c466c11bd5214f0ebae
CAIODIASDASILVA <127319215+CAIODIASDASILVA@users.noreply.github.com>
2024-10-02 09:39:46 -0300

    x

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const url = ser"https://servicodados.ibge.gov.br/api/v3/agregados/7524/periodos/198701/variaveis/29?localidades=N1[all]&classificacao=12716[115236
+const url = "https://servicodados.ibge.gov.br/api/v3/agregados/7524/periodos/198701/variaveis/29?localidades=N1[all]&classificacao=12716[115236"
 
 async function visualizarDados(){const res = await fetch(url)
 
