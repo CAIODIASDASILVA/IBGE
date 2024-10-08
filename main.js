905e287e14bf0d3a51e8f160d5ac8a6b317a5067
CAIODIASDASILVA <127319215+CAIODIASDASILVA@users.noreply.github.com>
2024-10-08 08:01:27 -0300

    d

--- main.js
+++ main.js
@@ -14,13 +14,17 @@ resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
 resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
 console.log(resultado_dados, resultado_regiao)
 
+let genero = dados[0]['resultados'][0]['classificacoes'][0]['categoria']
+Object.keys(resultado_dados1)[0]
+
+
 const paragrafo = document.createElement('p')
   paragrafo.classList.add('graficos--container__texto')
   paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em ${tipo_dados}.`
 
   const container = document.getElementById('graficos--container')
   container.appendChild(paragrafo)
-  
+
 }
 
 visualizarDados()
