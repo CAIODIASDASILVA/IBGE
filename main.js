70af30682ae151a14474fd376a58b28faad300d9
CAIODIASDASILVA <127319215+CAIODIASDASILVA@users.noreply.github.com>
2024-10-08 07:46:22 -0300

    ....

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const url = "https://servicodados.ibge.gov.br/api/v3/agregados/7524/periodos/198701/variaveis/29?localidades=N1[all]&classificacao=12716[115236"
 
-async function visualizarDados(){const res = await fetch(url)
+async function visualizarDados(){
+    
+    const res = await fetch(url)
 
 const dados = await res.json()
+
+regiao = 0
+
+resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
+resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
+console.log(resultado_dados, resultado_regiao)
 
 const tipo_dados = dados[0]['variavel']
 console.log(tipo_dados)
